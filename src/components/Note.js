4cd1d2d5db873b4701a67ea00e5d821659452aaa
nikkif24 <63017373+nikkif24@users.noreply.github.com>
2021-05-14 16:41:07 -0700

    Add files via upload

---
+++ src/components/Note.js
@@ -0,0 +1,11 @@
+const Note = ({title, onAdd, showAdd}) => {
+
+
+        return (
+            <header className='header'>
+               <h5>Double click to show "in progress" status with greens sidebar</h5> 
+            </header>
+        )
+        }
+
+export default Note
