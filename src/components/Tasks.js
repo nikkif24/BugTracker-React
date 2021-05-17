6792c9eb346c8479acd124a1480a0566dce829ba
nikkif24 <63017373+nikkif24@users.noreply.github.com>
2021-05-16 19:38:38 -0700

    Update Tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,6 +1,7 @@
 import Task from './Task'
 const Tasks = ({tasks, onDelete, onToggle}) => {
     return (
+        'Double click to show "in progress" status with greens sidebar'
         <>
          {tasks.map((task, index) => (
          <Task key={index} task={task} 
