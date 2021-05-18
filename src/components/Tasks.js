5c33399883d068b8b4c585d66b8a80398f465d80
nikkif24 <63017373+nikkif24@users.noreply.github.com>
2021-05-17 18:58:42 -0700

    Update Tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,13 +1,17 @@
 import Task from './Task'
+import Note from './Note'
+
 const Tasks = ({tasks, onDelete, onToggle}) => {
     return (
         <>
+        <Note />
          {tasks.map((task, index) => (
          <Task key={index} task={task} 
          onDelete={onDelete} 
          onToggle={onToggle}
          />
          ))}
+         
         </>
     )
 }
