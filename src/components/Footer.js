6c78c291cdb6264cfcf0d62f89a9430e5851c020
nikkif24 <63017373+nikkif24@users.noreply.github.com>
2021-05-17 18:57:51 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,8 @@
-import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
         <footer>
             <p>Copyright &copy; 2021</p>
-            <Link to='/about'>About</Link>
         </footer>
     )
 }
